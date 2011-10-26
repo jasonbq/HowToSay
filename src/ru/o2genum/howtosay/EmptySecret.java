6ac07b7f7ce25f6c80362188d0fb449a056560b9
Andrey Moiseev <o2@nm.ru>
2011-10-26 20:37:14 +0400

    Added an example of Secret.java

---
+++ src/ru/o2genum/howtosay/EmptySecret.java
@@ -0,0 +1,28 @@
+/*
+    Copyright (c) 2011, Andrey Moiseev
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may
+    not use this file except in compliance with the License. You may obtain
+    a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package ru.o2genum.howtosay;
+
+/**
+ * Secret API key. MUST NOT BE STORED BY VCS!
+ * This file is an example. Copy it to Secret.java and
+ * set apiKey variable to your key. Don't remove this file itself!
+ * Don't share your API key!
+ *
+ * @author Andrey Moiseev
+ */
+
+public class Secret {
+    // public static final String apiKey = ""; // Uncomment and set apiKey
+}
